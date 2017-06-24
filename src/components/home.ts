fb1f9cee7efed1bb0830bad8838bd2db87d4f51b
Shui Zong <shuaitong.zong@gmail.com>
2017-06-24 17:25:06 +0200

    Use Tachyons for CSS. Clean up

--- src/components/home.ts
+++ src/components/home.ts
@@ -3,68 +3,44 @@ import m from 'mithril';
 function view() {
     return (
     <div>
-    <h1>Online Nachrichten</h1>
-    <hr noshade size="3" />
-    <div class="button-group">
-        <button type="button" class="button">Nachrichten</button>
-        <button type="button" class="button">Ratgeber</button>
-        <button type="button" class="button">Sport</button>
-        <button type="button" class="button">Wissen</button>
-        <button type="button" class="button">Kultur</button>
-        <button type="button" class="button">Unterhaltung</button>
-    </div>
-    <div class="Spalten">    
-        <div class="window1">Nachrichten
-            <hr noshade size="1" />
-            <br />
-            <h2></h2>
-        </div>
-        <div class="window2">
-            <div class="head">Sport</div>
-            <hr noshade size="1" />
-            <br />
-            <h2></h2>
-        </div>   
-        <div class="window3">
-            <div class="head">Ratgeber</div>
-            <hr noshade size="1" />
-            <h2></h2>
-            <br/>
-        </div>
-        <div class="window4">
-            <div class="head">Wissen</div>
-            <hr noshade size="1" />
-            <h2></h2>
-        </div>
-        <div class="window5">
-            <div class="head">Kultur</div>
-        </div>
-    </div>
-    </div>
+
+<nav class="db dt-l w-100 border-box pa3 ph5-l">
+  <a class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+    <img src="http://tachyons.io/img/logo.jpg" class="dib w2 h2 br-100" alt="Site Name" />
+  </a>
+  <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Blah">Blah</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="How it Works">How it Works</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Blog">Blog</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Press">Press</a>
+    <a class="link dim dark-gray f6 f5-l dib" href="#" title="Contact">Contact</a>
+  </div>
+</nav>
+
+
+
+
+<div class="cf">
+  <div class="fl w-100 w-20-ns tc pv5 bg-black-05">
+    <h1>Nachrichten</h1>
+        <div>hallo</div>
+    
+  </div>
+  <div class="fl w-100 w-20-ns tc pv5 bg-black-10">
+    Ratgeber
+  </div>
+  <div class="fl w-100 w-20-ns tc pv5 bg-black-20">
+    Sport
+  </div>
+  <div class="fl w-100 w-20-ns tc pv5 bg-black-10">
+    Kultur
+  </div>
+  <div class="fl w-100 w-20-ns tc pv5 bg-black-05">
+    Unterhaltung
+  </div>
+</div>
+</div>
     );
 }
 
 export const Home = { view };
-
-/*
-            <?php
-		$xml = file_get_contents('http://www.swr.de/export/rss/swraktuell');
-		$object = simplexml_load_string($xml);	
-		//print_r($object);
-		
-		foreach ($object->{'channel'}->{'item'} as $item) {
-			//print_r($item);
-			echo $item->{'title'};
-			echo '<br>';
-			echo $item->{'pubDate'};
-			echo '<br>';
-			echo $item->{'guid'};
-			echo '<br>';
-			echo $item->{'link'};
-			echo '<br>';
-			echo '<br>';
-		}
-		?>
-*/
-
-
