d5855399041db014586e9ae844fda5fae5f4574d
Shui Zong <shuaitong.zong@gmail.com>
2017-06-24 18:09:39 +0200

    Request data from server

--- src/backend.js
+++ src/backend.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 
-/*
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/api/v0/news', function (req, res) {
+
+  var data = {
+    "news": [
+      {"headline": "This is a news headline", "body": "..."},
+      {"headline": "This is another news headline", "body": "Nice"},      
+    ]
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
 });
-*/
 
 app.use('/', express.static('public'));
 
--- src/components/home.ts
+++ src/components/home.ts
@@ -1,9 +1,48 @@
 import m from 'mithril';
 
-function view() {
+interface NewsItem {
+    headline: String;
+    body: String;
+}
+
+interface NewsApi {
+    news: NewsItem[];
+}
+
+function fetchNews(): Promise<NewsApi> {
+    // Returns a promise object
+    return m.request({
+        method: "GET",
+        url: "/api/v0/news"
+    });
+}
+
+function controller() {
+    return {
+        news: fetchNews()
+    }
+}
+
+function renderNewsItem(item: NewsItem) {
+        return <div>
+           <b>{item.headline}</b>
+           <div>{item.body}</div>
+        </div>;
+}
+
+function renderNews(newsItems: NewsItem[]) {
+    let renderedNews = [];
+    for (let item of newsItems) {
+        renderedNews.push(renderNewsItem(item));
+    }
+    return renderedNews;
+}
+
+function view(ctrl) {
+    let news: NewsItem[] = ctrl.news().news; // this waits until the data becomes available
+
     return (
     <div>
-
 <nav class="db dt-l w-100 border-box pa3 ph5-l">
   <a class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
     <img src="http://tachyons.io/img/logo.jpg" class="dib w2 h2 br-100" alt="Site Name" />
@@ -17,14 +56,10 @@ function view() {
   </div>
 </nav>
 
-
-
-
 <div class="cf">
   <div class="fl w-100 w-20-ns tc pv5 bg-black-05">
     <h1>Nachrichten</h1>
-        <div>hallo</div>
-    
+        { renderNews(news) }
   </div>
   <div class="fl w-100 w-20-ns tc pv5 bg-black-10">
     Ratgeber
@@ -43,4 +78,4 @@ function view() {
     );
 }
 
-export const Home = { view };
+export const Home = { controller, view };
